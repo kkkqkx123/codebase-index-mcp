35ba419e104eb76a573ecbee5f0e1a079d604147
Admin <3353074868@qq.com>
2025-08-30 15:01:58 +0800

    fix import path error

--- src/services/parser/test-imports.ts
+++ src/services/parser/test-imports.ts
@@ -1,5 +1,5 @@
-import { ConfigService } from '../config/ConfigService';
-import { LoggerService } from '../core/LoggerService';
-import { ErrorHandlerService } from '../core/ErrorHandlerService';
+import { ConfigService } from '../../config/ConfigService';
+import { LoggerService } from '../../core/LoggerService';
+import { ErrorHandlerService } from '../../core/ErrorHandlerService';
 
 console.log('Imports work correctly');
